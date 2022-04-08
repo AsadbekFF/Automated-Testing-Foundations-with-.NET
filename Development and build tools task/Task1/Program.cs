5c1c8a88316d546afa9f015d3c71782c7cb9fc02
AsadbekFF <79692640+AsadbekFF@users.noreply.github.com>
2022-04-08 08:42:16 +0500

    Maximum number of unequal consecutive characters in a string.

---
+++ Development and build tools task/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            int sum = 0;
+            int max = 0;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] != str[i - 1])
+                {
+                    sum++;
+                }
+                else
+                {
+                    if (sum > max)
+                    {
+                        max = sum;
+                        sum = 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
