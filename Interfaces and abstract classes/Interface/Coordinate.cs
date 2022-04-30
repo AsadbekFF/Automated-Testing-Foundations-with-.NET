00a69085292b9f0410560aef1c70599ffccc1a3c
AsadbekFF <79692640+AsadbekFF@users.noreply.github.com>
2022-04-30 12:37:24 +0500

    Update Coordinate.cs

--- Interfaces and abstract classes/Interface/Coordinate.cs
+++ Interfaces and abstract classes/Interface/Coordinate.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,22 @@ namespace Interface
 {
     public struct Coordinate
     {
-        public Coordinate(int initialRange, int endRange)
-        {
-            Random random = new();
-            x = random.Next(0, 100);
-            y = random.Next(0, 100);
-            z = random.Next(0, 100);
-        }
 
         public int x;
         public int y;
         public int z;
+
+        public static Coordinate GenerateCoordination()
+        {
+            Random random = new();
+            return new Coordinate
+            {
+                x = random.Next(),
+                y = random.Next(),
+                z = random.Next()
+            };
+
+            // Generating non-negative Coordinate
+        }
     }
 }
